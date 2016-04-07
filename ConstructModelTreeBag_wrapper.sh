d32bb7e53225ba6e612cb62f7c06e44289d6091e
unknown <feczko@MACC061.ohsum01.ohsu.edu>
2016-04-07 11:15:04 -0700

    version 0.22.2 -- modified wrappers to incorporate new changes to program and enable the use of a parameter file

--- ConstructModelTreeBag_wrapper.sh
+++ ConstructModelTreeBag_wrapper.sh
@@ -1,45 +1,41 @@
 #! /bin/bash
 
-#required parameters: 
-#groupxpath is the full path to the .mat file storing the groupx data
-#groupxvar is the name of the variable within the .mat file that stores groupx data
-group1path=$1;shift;
-group1var=$1;shift;
-group2path=$1;shift;
-group2var=$1;shift;
-#optional parameters, defaults are given below
-datasplit=$1;shift;
-nreps=$1;shift;
-ntrees=$1;shift;
-nperms=$1;shift;
-filename=$1;shift;
-estimate_trees=$1;shift;
-weight_trees=$1;shift;
-trim_features=$1;shift;
-nfeatures=$1;shift;
-fisher_z_transform=$1;shift;
-disable_treebag=$1;shift;
-holdout=$1;shift;
-holdout_data=$1;shift;
-group_holdout=$1;shift;
-proxsublimit_set=$1;shift;
-proxsublimit_num=$1;shift;
+#ConstructModelTreeBag_wrapper.sh requires a ParamFile as an input (e.g. ConstructModelTreeBag_wrapper.sh TreeBagParamFile_example.bash). See the TreeBagParamFile_example.bash for more information on available parameters.
+source $1
+#parameters set from the TreeBagParamFile
+if $use_group2_data; then group2_data="struct('path','"${group2path}"','variable','"${group2var}"')"; else group2_data=0; fi
+if $estimate_trees; then estimate_trees='EstimateTrees'; else estimate_trees='NONE'; fi
+if $weight_trees; then weight_trees='WeightForest'; else weight_trees='NONE'; fi
+if $trim_features; then trim_features='TrimFeatures'; else trim_features='NONE'; fi
+if $fisher_z_transform; then fisher_z_transform='FisherZ'; else fisher_z_transform='NONE'; fi
+if $disable_treebag; then disable_treebag='TreebagsOff'; else disable_treebag='NONE'; fi
+if $holdout; then holdout='Holdout'; else holdout='NONE'; fi
+if $npredictors; then npredictors='npredictors'; else npredictors='NONE'; fi
+if $estimate_predictors; then estimate_predictors='EstimatePredictors'; else estimate_predictors='NONE'; fi
+if $regression; then regression='Regression'; if $outcome_is_struct; then group1outcome="struct('path','"${group1outcome_path}"','variable','"${group1outcome_var}"')"; group2outcome="struct('path','"${group2outcome_path}"','variable','"${group2outcome_var}"')"; else group1outcome=$group1outcome_num; group2outcome=$group2outcome_num; fi; else regression='NONE'; group1outcome=0; group2outcome=0; fi
+if $surrogate; then surrogate='surrogate'; else surrogate='NONE'; fi
 #If missing parameters, set defaults
 datasplit=${datasplit:-0.9}
 ntrees=${ntrees:-1000}
 nreps=${nreps:-1000}
 nperms=${nperms:-1}
 filename=${filename:-'thenamelessone'}
-estimate_trees=${estimate_trees:-'EstimateTrees'}
-weight_trees=${weight_trees:-'WeightForest'}
-trim_features=${trim_features:-'blah'}
+estimate_trees=${estimate_trees:-'NONE'}
+weight_trees=${weight_trees:-'NONE'}
+trim_features=${trim_features:-'NONE'}
 nfeatures=${nfeatures:-0}
-fisher_z_transform=${fisher_z_transform:-'blahblahblah'}
+fisher_z_transform=${fisher_z_transform:-'NONE'}
 disable_treebag=${disable_treebag:-'TreebagsOff'}
-holdout=${holdout:-'yada'}
-holdout_data=${holdout_data:-'yadayada'}
+holdout=${holdout:-'NONE'}
+holdout_data=${holdout_data:-'NONE'}
 group_holdout=${group_holdout:-0}
-proxsublimit_set=${proxsublimit_set:-'ProximitySubLimit'}
 proxsublimit_num=${proxsublimit_num:-500}
+npredictors=${npredictors:-'NONE'}
+num_predictors=${num_predictors:-0}
+estimate_predictors=${estimate_predictors:-'NONE'}
+regression=${regression:-'NONE'}
+surrogate=${surrogate:-'NONE'}
+group1outcome=${group1outcome:-0}
+group2outcome=${group2outcome:-0}
 #Construct the model, which will save outputs to a filename.mat file
-matlab14b -nojvm -nodisplay -nosplash -singleCompThread -r "addpath('/group_shares/PSYCH/code/testing/analyses/treebag') ; ConstructModelTreeBag(struct('path','"${group1path}"','variable','"${group1var}"'),struct('path','"${group2path}"','variable','"${group2var}"'),"$datasplit","$nreps","$ntrees","$nperms",'"${filename}"','"${estimate_trees}"','"${weight_trees}"','"${trim_features}"',"$nfeatures",'"${fisher_z_transform}"','"${disable_treebag}"','"${holdout}"','"${holdout_data}"',"$group_holdout",'"${proxsublimit_set}"',"$proxsublimit_num") ; exit"
+matlab14b -nojvm -nodisplay -nosplash -singleCompThread -r "addpath('/group_shares/FAIR_LAB2/Projects/FAIR_users/Feczko/projects/Analysis') ; ConstructModelTreeBag(struct('path','"${group1path}"','variable','"${group1var}"'),"$group2_data","$datasplit","$nreps","$ntrees","$nperms",'"${filename}"',"$proxsublimit_num",'"${estimate_trees}"','"${weight_trees}"','"${trim_features}"',"$nfeatures",'"${fisher_z_transform}"','"${disable_treebag}"','"${holdout}"','"${holdout_data}"',"$group_holdout",'"${npredictors}"',"$num_predictors",'"${surrogate}"','"${regression}"',"$group1outcome","$group2outcome") ; exit"
