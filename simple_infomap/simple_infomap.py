a0a45175adcb8aec39031ec8626a3aad9d419af5
unknown <demeter@HRCC095.ohsum01.ohsu.edu>
2016-04-27 12:59:28 -0700

    Updated to use new version of infomap. Also added arguments to run undirected or directed and also to manually input the number of infomap attempts to be run.

--- simple_infomap/simple_infomap.py
+++ simple_infomap/simple_infomap.py
@@ -4,7 +4,7 @@
 # Originally written by Damion Demeter, 09.03.2015
 #
 # Infomap Community Detection from Matrix (last revision's modified date)
-last_modified = 'Last Modified by Damion Demeter, 09.03.15'
+last_modified = 'Last Modified by Damion Demeter, 04.25.16'
 
 import argparse,datetime,getpass,math,numpy,os,random,subprocess,sys,time
 import scipy.io as sio
@@ -17,6 +17,14 @@ prog_descrip = '""%(prog)s: Infomap Community Detection from Matrix Script. Use 
 def main(argv=sys.argv):
     arg_parser = argparse.ArgumentParser(description=prog_descrip)
 
+    arg_parser.add_argument('-a', metavar='INFOMAP_ATTEMPTS', action='store', required=False, type=int, default=5,
+                            help=('Infomap attempts. This will re-run infomap for precision. NOT Required. Default = 5'),
+                            dest='attempts'
+                           )
+    arg_parser.add_argument('-d', action='store_true', required=False, default=False,
+                            help=('Flag for using a DIRECTED Matrix.'),
+                            dest='dir_mat'
+                           )
     arg_parser.add_argument('-m', metavar='MATRIX_PATH', action='store', required=True, type=os.path.abspath,
                             help=('Full path to your correlation matrix. Required.'),
                             dest='corr_matrix'
@@ -28,13 +36,18 @@ def main(argv=sys.argv):
     arg_parser.add_argument('-p', metavar='PERCENT_THRESHOLD', nargs='?', action='store', required=False, type=float, default=1.0,
                             help=('Percent Threshold (Top Connections) Use 0.0X format. :: Default is 1.0 assuming thresholding already performed.'),
                             dest='perc_thresh'
-                           )                                              
+                           )
+    arg_parser.add_argument('-u', action='store_true', required=False, default=True,
+                            help=('Flag for using an UNDIRECTED Matrix.'),
+                            dest='undir_mat'
+                           )                           
     args = arg_parser.parse_args()
     #################################################
     ## Script Argument Verification and Assignment ##
     #################################################
     print '\n--------------------- setup info ---------------------------------'
 
+    ## VERIFY MATRIX/PATH EXISTS ##
     if os.path.isfile(args.corr_matrix):
         Corr_mat_path = args.corr_matrix
         print 'Running infomap_undir community detection on the following matrix:'
@@ -42,7 +55,7 @@ def main(argv=sys.argv):
     else:
         print 'The matrix path you specified is not a file. Exiting...'
         sys.exit()
-    ###
+    ## VERIFY OUTPUT DIRECTORY EXISTS ##
     if os.path.isdir(args.out_dir):
         out_dir = args.out_dir
         print 'Output directory ("community_detection") will be created here:'
@@ -50,7 +63,24 @@ def main(argv=sys.argv):
     else:
         print 'The output directory you specified does not exist. Exiting...'
         sys.exit()
-    ###        
+    ## VERIFY MATRIX DIRECTION ##
+    if args.dir_mat == True and args.undir_mat == True:
+        print '\nYou have marked your matrix as BOTH directed AND undirected. Cannot be both. Exiting...'
+        sys.exit()
+    elif args.dir_mat == False and args.undir_mat == False:
+        print '\nYou must use either the undirected or directed matrix flag. See help for more info. Exiting...'
+        sys.exit()
+    elif args.dir_mat == True:
+        print 'Processing matrix as DIRECTED.'
+        mat_type = 'dir'
+        mat_type_string = 'DIRECTED'
+        mat_command = '-d'
+    elif args.undir_mat == True:
+        print 'Processing matrix as UNDIRECTED.'
+        mat_type = 'undir'
+        mat_type_string = 'UNDIRECTED'
+        mat_command = '-u'
+    ## THRESHOLD PERCENTAGE ##        
     if args.perc_thresh == 1.0:
         print 'Using default PERCENT THRESHOLD of 1.0 (100% of connections)'
         perc_thresh = float(args.perc_thresh)
@@ -70,6 +100,11 @@ def main(argv=sys.argv):
     time.sleep(1)
     today_date = datetime.datetime.now().strftime('%m%d%Y')
     curr_user = getpass.getuser()
+
+#####################    
+    def natural_key(string):
+        import re
+        return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string)]    
     
 #####################
     def matrix_prep():
@@ -82,8 +117,12 @@ def main(argv=sys.argv):
         mat_name = mat_file.split('.')[0]
         mat_file = sio.loadmat(Corr_mat_path)
 
-        Corr_mat = mat_file[mat_name]
-        
+        for key in mat_file.keys():
+            if not '__' in key:
+                matrix_key = str(key)
+
+        Corr_mat = mat_file[matrix_key]
+ 
         voxel_count = numpy.shape(Corr_mat)[0]
         num_of_conn = int(math.ceil(perc_thresh*((voxel_count*(voxel_count-1))/2)))
         
@@ -121,7 +160,7 @@ def main(argv=sys.argv):
             f.write('*Vertices ' + str(len(Corr_mat)) + '\n')
             for i in range(1,len(Corr_mat)+1):
                 f.write(' ' + str(i) + ' "v' + str(i) + '"\n')
-            f.write('*Arcs\n')
+            #f.write('*Arcs\n')
             f.write('*Edges\n')
             
             for i in range(0,len(Corr_mat)):
@@ -130,27 +169,33 @@ def main(argv=sys.argv):
                         f.write(' ' + str(i+1) + ' ' + str(j+1) + ' ' + "{:1.6f}".format(Corr_mat[i,j]) + '\n')
         f.close()
         
-        ## RUNNING infomap_undir USING adj.net file created ##
-        print 'Running infomap community detection...'
-        command = 'infomap_undir'    
-        random_num = random.randint(0,100000000)
-        comm = command + ' ' + str(random_num) + ' ' + dst_file + ' 1'
-    
-        subprocess.call(comm, shell=True)   
-    
+        ## RUNNING *NEW* infomap using adj.net file created ##
+        infomap = '/group_shares/PSYCH/code/external/utilities/infomap/Infomap'
+        print 'Running infomap community detection on your ' + mat_type_string + ' matrix...'
+        
+        rand_num = random.randint(1,9999)
+        
+        infomap_comm = ' '.join([infomap,dst_file,output_dir,'--clu --map --tree --bftree -i pajek',mat_command,'-s',str(rand_num),'-N',str(args.attempts)])
+             
+        subprocess.call(infomap_comm, shell=True)
+        
         ## OPEN CLU FILE, MAKE LIST, AND SAVE COPY ##
         clu_path = dst_file.replace('net','clu')
         
         group_comm_vect = os.path.join( output_dir, mat_name + '_' + perc_thresh_str + '_thresh_comms.txt' )
         
+        clu_tuples = []
         f = open(clu_path,'r')
-        comm_lines = [line.rstrip('\r\n') for line in f]
-        comms_list = comm_lines[1:]
+        for line in f:
+            split_line = line.split(' ')
+            if len(split_line) == 3:
+                line_tuple = (int(split_line[0]),split_line[1])
+                clu_tuples.append(line_tuple)
         f.close()
-        
+
         with open(group_comm_vect, 'w') as vect_out:
-            for c in comm_lines[1:]:
-                vect_out.write(str(c) + '\n')            
+            for c in sorted(clu_tuples):
+                vect_out.write(str(c[1]) + '\n')
         vect_out.close()
 
 ################# RUNNING FUNCTIONS #################
@@ -159,10 +204,7 @@ def main(argv=sys.argv):
     
     infomap(Corr_mat,mat_name)
     
-    
-    
-    
-    
+
     full_runtime = time.time() - start_time
     print 'Full Script Runtime: ', datetime.timedelta(seconds=full_runtime), '\n'
 if __name__ == '__main__':
