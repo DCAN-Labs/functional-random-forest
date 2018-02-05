aeaa5484d4c88c5069d6e4f3589a5b5d7eb4b605
Philip Robinson <robinsph@ohsu.edu>
2018-02-05 18:37:30 +0000

    Update VisualizeTreeBagResults_example.bash

--- VisualizeTreeBagResults_example.sh
+++ VisualizeTreeBagResults_example.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 group1path=/group_shares/FAIR_LAB2/Projects/FAIR_users/Feczko/projects/Long_ADHD_OHSU/MBM_all.mat #path and filename where group1's data is located
 group1var=X_CC #the name of the variable within group1's matrix (.mat) file. This should represent a 2D matrix
 use_group2_data=true #if set to true, the dataset for the second group will be specified in a separate file, if set to false, the first group's dataset will be randomly split into two groups. Set to false when doing regression
